8a77371a01718813b97f7aa83cc40aac8fae144e
aboXmsa3d <aboXmsa3d@outlook.com>
2020-06-19 16:54:37 +0300

    apply the 'PEP 8 style'

--- backend/src/api.py
+++ backend/src/api.py
@@ -5,21 +5,15 @@ import json
 from flask_cors import CORS
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth 
+from .auth.auth import AuthError, requires_auth
 
-'''
-cd backend/src
-set FLASK_APP=api.py
-$env:FLASK_APP = "api.py"
-flask run --reload
-'''
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
+    uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
@@ -28,18 +22,12 @@ db_drop_and_create_all()
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ endpoints ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-'''
-@TODO implement endpoint GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks', methods=['GET'])
 def drinks():
 
     all_drinks = Drink.query.all()
-    drinks=[]
+    drinks = []
 
     for drink in all_drinks:
         drinks.append(drink.short())
@@ -49,20 +37,15 @@ def drinks():
         "drinks": drinks
     })
 
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-'''
-@TODO implement endpoint GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
 def drinks_detail(payload):
 
     all_drinks = Drink.query.all()
-    drinks=[]
+    drinks = []
 
     for drink in all_drinks:
         drinks.append(drink.long())
@@ -72,15 +55,9 @@ def drinks_detail(payload):
         "drinks": drinks
     })
 
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-'''
-@TODO implement endpoint POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def add_drink(payload):
@@ -93,8 +70,8 @@ def add_drink(payload):
     try:
         drink = Drink(
             title=new_title,
-            recipe=json.dumps(new_recipe) # write it as String
-            )
+            recipe=json.dumps(new_recipe)  # write it as String
+        )
 
         drink.insert()
 
@@ -105,22 +82,13 @@ def add_drink(payload):
 
     except BaseException:
         abort(422)
-   
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-'''
-@TODO implement endpoint PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
-def edit_drink(payload , drink_id):
+def edit_drink(payload, drink_id):
 
     drink = Drink.query.get(drink_id)
 
@@ -137,7 +105,7 @@ def edit_drink(payload , drink_id):
         if new_title is not None:
             drink.title = new_title
         if new_recipe is not None:
-            drink.recipe = json.dumps(new_recipe) # write it as String
+            drink.recipe = json.dumps(new_recipe)  # write it as String
 
         drink.update()
 
@@ -149,19 +117,12 @@ def edit_drink(payload , drink_id):
     except BaseException:
         abort(422)
 
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-'''
-@TODO implement endpoint DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
-def delete_drink(payload , drink_id):
+def delete_drink(payload, drink_id):
 
     drink = Drink.query.get(drink_id)
 
@@ -179,7 +140,6 @@ def delete_drink(payload , drink_id):
 
     except BaseException:
         abort(422)
-
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Error Handlers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
@@ -212,7 +172,7 @@ def bad_request(error):
 
 
 @app.errorhandler(401)
-def Unauthorized (error):
+def Unauthorized(error):
     return jsonify({
         'success': False,
         'error': 401,
@@ -220,16 +180,11 @@ def Unauthorized (error):
     }), 401
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
 @app.errorhandler(AuthError)
 def handle_auth_error(ex):
     response = jsonify(ex.error)
     response.status_code = ex.status_code
     return response
-
 
 
 if __name__ == '__main__':
